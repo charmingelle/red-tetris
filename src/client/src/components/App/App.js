4a27ec5ebee188d5c81d93ca815195dfdbafd7f8
Ganna Revenko <gannar@wix.com>
2019-10-21 23:00:05 +0300

    add WebSocket

--- src/client/src/components/App/App.js
+++ src/client/src/components/App/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import './App.css';
+import socketIOClient from 'socket.io-client';
 
-export default () => {
+const checkTestRequestResult = () =>
   fetch('/users', { method: 'GET', credentials: 'include' })
     .then(res => res.json())
     .then(console.log)
     .catch(console.error);
+
+export default () => {
+  const login = 'user';
+
+  socketIOClient({
+    query: `login=${login}`,
+  });
+
+  checkTestRequestResult();
 
   return <div className="App">Test App</div>;
 };
--- src/server/index.js
+++ src/server/index.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 const post = 5000;
+const http = app.listen(post, () =>
+  console.log(`Server is running on port ${5000}`),
+);
+const io = require('socket.io')(http);
 
 app.get('/users', (req, res) => res.json('Hi!'));
 
-app.listen(post, () => console.log(`Server is running on port ${5000}`));
+io.use((socket, next) => {
+  console.log(socket.id, socket.request._query.login);
+  next();
+});
