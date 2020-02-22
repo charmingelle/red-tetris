3d9691e165d6e98364d00e30e6a88628907c3c91
Ganna Revenko <gannar@wix.com>
2020-02-22 16:00:16 +0200

    add start command for production

--- src/server/index.js
+++ src/server/index.js
@@ -1,9 +1,17 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const post = 5000;
 const http = app.listen(post, () =>
   console.log(`Server is running on port ${5000}`),
 );
+
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 const Connection = require('./classes/Connection');
 
 new Connection(require('socket.io')(http));
